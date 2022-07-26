38ce2e83fc101e3f14fc159d74675196ed08e86e
Daniel Sobit <sobitdaniel@gmail.com>
2022-07-26 15:05:21 +0300

    sidebar initialization and styling

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import sidebar from "sidebar.css"
+
+function Sidebar() {
+
+    return(
+          /* twitter's side bar has a div with both icon and text*/
+        <div className="sidebar">
+           <Icon/>
+           <h2>text</h2>
+        </div>
+    )
+}
