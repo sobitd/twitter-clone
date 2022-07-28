ac8ec0d1c33157953c923965e28c4422937919a2
Daniel Sobit <sobitdaniel@gmail.com>
2022-07-28 14:46:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import MainFeedBox from './Components/MainFeedBox';
 
 function App() {
   return (
     <div className="app">
       <Sidebar/>
+      <MainFeedBox />
+
     </div>
   );
 }
--- src/Components/MainFeedBox.js
+++ src/Components/MainFeedBox.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CreateTweet from "./CreateTweet";
+import DispTweets from "./DispTweets";
+import "../App.css";
 
 const MainFeedBox = () => {
-  return <div></div>;
+  const [displayTweets, setDisplayTweets] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/Tweets")
+      .then((res) => res.json())
+      .then((displayTweets) => setDisplayTweets(displayTweets));
+  }, []);
+
+  return (
+    <div className="feed">
+      <div className="feed-header">
+        <h2>Home</h2>
+        <CreateTweet />
+      </div>
+      {displayTweets.map((displayTweet) => (
+        <DispTweets displayTweets={displayTweet} />
+      ))}
+    </div>
+  );
 };
 export default MainFeedBox;
