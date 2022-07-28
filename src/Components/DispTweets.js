f6e071e86252ba44dc737ea8c065d84d7aa0c130
Daniel Sobit <sobitdaniel@gmail.com>
2022-07-28 10:15:17 +0300

    update DispTweets component

--- src/Components/DispTweets.js
+++ src/Components/DispTweets.js
@@ -1,6 +1,55 @@
 import React from "react";
 import { Avatar } from "@mui/material";
 import "../App.css";
-import { ChatBubbleOutline, FavoriteBorder, Publish, Repeat, VerifiedUser } from "@mui/icons-material";
+import {
+  ChatBubbleOutline,
+  FavoriteBorder,
+  Publish,
+  Repeat,
+  VerifiedUser,
+  Delete,
+  Update,
+} from "@mui/icons-material";
 
-const 
+const DispTweets = ({
+  avatar,
+  text,
+  image,
+  profileName,
+  username,
+  verified,
+}) => {
+  return (
+    <div className="display-tweets">
+      <div className="user-avatar">
+        <Avatar src={avatar} />
+      </div>
+      <div className="tweet-body">
+        <div className="tweet-header">
+          <div className="tweet-header-text">
+            <h3>
+              {profileName}{" "}
+              <span className="verified-badge">
+                {verified && VerifiedUser} {username}
+              </span>
+            </h3>
+          </div>
+          <div className="tweet-body-text">
+            <p>{text}</p>
+          </div>
+        </div>
+        <img src={image} alt="" />
+        <div className="tweet-footer">
+          <ChatBubbleOutline fontSize="small" />
+          <Repeat fontSize="small" />
+          <FavoriteBorder fontSize="small" />
+          <Update fontSize="small" />
+          <Delete fontSize="small" />
+          <Publish fontSize="small" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DispTweets;
