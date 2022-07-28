747ec498331d86c77c705a15ad09c0dab0244d7c
Daniel Sobit <sobitdaniel@gmail.com>
2022-07-28 09:39:23 +0300

    add twitter ui decpendencies from Material UI

--- src/Components/DispTweets.js
+++ src/Components/DispTweets.js
@@ -1,2 +1,6 @@
 import React from "react";
 import { Avatar } from "@mui/material";
+import "../App.css";
+import { ChatBubbleOutline, FavoriteBorder, Publish, Repeat, VerifiedUser } from "@mui/icons-material";
+
+const 
