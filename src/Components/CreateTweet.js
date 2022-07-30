c2781ef545757163b77454a7603a7f6e76b62a8b
Daniel Sobit <sobitdaniel@gmail.com>
2022-07-30 12:09:13 +0300

    fetch function

--- src/Components/CreateTweet.js
+++ src/Components/CreateTweet.js
@@ -3,42 +3,75 @@ import "../App.css";
 import { Avatar, Button } from "@mui/material";
 
 const CreateTweet = (props) => {
-  const [newTweet, setNewTweet] = useState({
-    caption: "",
-  });
+  const [caption, setCaption] = useState("");
+  const [name, setName] = useState("Kimani");
+  const [username, setUsername] = useState("@kimani");
+  const [image, setImage] = useState(
+    "https://i.pinimg.com/564x/b2/bb/98/b2bb9888eebc1b602ec343441d3e5d8c.jpg"
+  );
+  const [time, setTime] = useState("");
 
-  function handleChange(event) {
-    setNewTweet(event.target.value);
-  }
+  const timePosted = () => {
+    const startTime = Date();
+    const currentTime = new Date();
+    const timeElapsed = (currentTime - startTime) / 1000;
+    return timeElapsed;
+  };
 
-  function handleCreateTweet(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
+
+    const postData = {
+      name: name,
+      username: username,
+      image: image,
+      caption: caption,
+      time: time,
+    };
 
     fetch("https://twitter-server-app.herokuapp.com/tweets", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        caption: newTweet.caption,
-      }),
-    });
-  }
+      body: JSON.stringify(postData),
+    })
+      .then((r) => r.json())
+      .then((newTweet) => console.log(newTweet));
+  };
 
   return (
     <div className="tweet-container">
-      <form onSubmit={handleCreateTweet}>
+      <form>
         {/* compose a new tweet */}
         <div className="create-tweet-box">
-          <Avatar src="https://i.pinimg.com/564x/b2/bb/98/b2bb9888eebc1b602ec343441d3e5d8c.jpg" />
           <input
-            value={newTweet}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="hidden"
+          />
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="hidden"
+          />
+          <input
+            value={time}
+            onChange={(e) => setTime(timePosted())}
+            type="hidden"
+          />
+          <Avatar src={image} onChange={(e) => setImage(e.target.value)} />
+          <input
             placeholder="What's happening?"
             type="text"
-            onChange={handleChange}
+            onChange={(e) => setCaption(e.target.value)}
           />
         </div>
-        <Button type="submit" className="create-tweet-button">
+        <Button
+          type="submit"
+          onClick={handleSubmit}
+          className="create-tweet-button"
+        >
           Tweet
         </Button>
       </form>
